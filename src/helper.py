eccb758a5f151de273b59ea36e4d0a154b04794e
Torben <59419684+entorb@users.noreply.github.com>
2025-10-15 22:01:50 +0200

    cleanup comments etc

--- src/helper.py
+++ src/helper.py
@@ -10,16 +10,18 @@ from streamlit.logger import get_logger
 if TYPE_CHECKING:
     from streamlit.navigation.page import StreamlitPage
 
+PATH_ON_WEBSERVER = "/home/entorb/korrekturleser"
+
 
 @st.cache_resource
 def get_shared_state() -> dict[str, str]:
     """
     Shared state dict.
 
-    Prefilled with ENV = where the app is running.
+    Prefilled with ENV (= where the app is running).
     """
     d: dict[str, str] = {}
-    d["ENV"] = "PROD" if Path("/home/entorb/korrekturleser").is_dir() else "Local"
+    d["ENV"] = "PROD" if Path(PATH_ON_WEBSERVER).is_dir() else "Local"
     return d
 
 
@@ -27,6 +29,8 @@ def init_dev_session_state() -> None:
     """Set session variables needed for local dev without login."""
     st.session_state["USER_ID"] = 1
     st.session_state["USERNAME"] = "Torben"
+    st.session_state["cnt_requests"] = 0
+    st.session_state["cnt_tokens"] = 0
 
 
 def get_logger_from_filename(file: str) -> Logger:
@@ -51,15 +55,3 @@ def create_navigation_menu() -> str:
     pg = st.navigation(lst)
     pg.run()
     return pg.url_path
-
-
-# def init_sentry() -> None:
-#     """Initialize Sentry exception tracking/alerting."""
-#     import sentry_sdk
-
-#     sentry_sdk.init(
-#         dsn=st.secrets["SENTRY_DSN"],
-#         environment=st.session_state["ENV"],
-#         send_default_pii=True,
-#         traces_sample_rate=0.0,
-#     )
