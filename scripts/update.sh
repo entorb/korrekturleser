e2184fbbf8622e6102ad1e7b04041fc8a8c3b5bb
Torben <59419684+entorb@users.noreply.github.com>
2025-10-16 15:11:43 +0200

    LLM: ollama only for dev, not prod

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,24 +1,14 @@
 #!/bin/sh
 cd $(dirname $0)/..
 
-# pyenv global 3.11
-# python -m pip install --upgrade pip
-# pip install --upgrade streamlit XlsxWriter
-# pip freeze >requirements-all.txt
-# grep -E "streamlit=|XlsxWriter=" requirements-all.txt >requirements.txt
-
-# update pyproject.toml
-# uv add -r requirements.txt
-# pyenv global 3.13
-
-uv remove pandas pyarrow google-genai mysql-connector-python ollama st_copy streamlit
-uv remove --dev ruff pre-commit pytest pytest-cov tomli-w watchdog
+uv remove pandas pyarrow google-genai mysql-connector-python st_copy streamlit
+uv remove --dev ruff ollama pre-commit pytest pytest-cov tomli-w watchdog
 
 uv lock --upgrade
 uv sync --upgrade
 
-uv add pandas==2.2.3 pyarrow==20.0.0 google-genai mysql-connector-python ollama st_copy streamlit
-uv add --dev ruff pre-commit pytest pytest-cov tomli-w watchdog
+uv add pandas==2.2.3 pyarrow==20.0.0 google-genai mysql-connector-python st_copy streamlit
+uv add --dev ruff ollama pre-commit pytest pytest-cov tomli-w watchdog
 
 uv lock --upgrade
 uv sync --upgrade
