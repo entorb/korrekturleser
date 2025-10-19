105353071648882a60f1f71a33c0049d61491546
Torben <59419684+entorb@users.noreply.github.com>
2025-10-19 12:07:10 +0200

    deployment of V2 works!

--- fastapi_app/gunicorn_conf.py
+++ fastapi_app/gunicorn_conf.py
@@ -1,14 +1,18 @@
 """Config file for Gunicorn for prod deployment on entorb.net."""
 
-# apply changes via
+# run manually
+# ~/.local/bin/gunicorn --config ~/korrekturleser/fastapi_app/gunicorn_conf.py
+# run via supervisorctl
 # supervisorctl restart korrekturleser-fastapi
+# see log
+# tail -f ~/logs/supervisord.log
 
 import os
 
 app_path = os.environ["HOME"] + "/korrekturleser/"
 
 # Gunicorn configuration
-wsgi_app = "fastapi_app:main:api"
+wsgi_app = "fastapi_app.main:app"
 bind = ":9002"
 chdir = app_path
 workers = 1
