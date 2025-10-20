ef3161befa4c1988a9b4577924c3811c0e2922f1
Torben <59419684+entorb@users.noreply.github.com>
2025-10-20 17:42:24 +0200

    cleanup leftovers

--- src/reports/r09_raus.py
+++
@@ -1,8 +0,0 @@
-"""Logout."""
-
-import streamlit as st
-
-st.title("Tschüss " + st.session_state["USERNAME"])
-
-for key in ["USER_ID", "USERNAME", "ai_response"]:
-    st.session_state.pop(key, None)
