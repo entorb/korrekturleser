42beb0fce61e48f461378cff65dfec4ca38e256b
Torben <59419684+entorb@users.noreply.github.com>
2025-11-04 06:49:00 +0100

    minor bug fixes: PWA icons, user stats

--- vite.config.ts
+++ vite.config.ts
@@ -73,40 +73,31 @@ export default defineConfig({
         start_url: '/korrekturleser-vue/',
         icons: [
           {
-            src: '/korrekturleser-vue/favicon.svg',
-            sizes: 'any',
-            type: 'image/svg+xml',
-            purpose: 'any maskable'
-          },
-          {
-            src: '/korrekturleser-vue/favicon-32x32.png',
-            sizes: '32x32',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
-            src: '/korrekturleser-vue/favicon-16x16.png',
-            sizes: '16x16',
-            type: 'image/png',
-            purpose: 'any'
-          },
-          {
             src: '/korrekturleser-vue/android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
-            purpose: 'any'
+            purpose: 'any maskable'
           },
           {
             src: '/korrekturleser-vue/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any'
+            purpose: 'any maskable'
           },
           {
             src: '/korrekturleser-vue/apple-touch-icon.png',
             sizes: '180x180',
-            type: 'image/png',
-            purpose: 'any'
+            type: 'image/png'
+          },
+          {
+            src: '/korrekturleser-vue/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png'
+          },
+          {
+            src: '/korrekturleser-vue/favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png'
           }
         ]
       },
