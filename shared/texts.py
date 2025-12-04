b0c61a07bb820e71d46ccc119872e676c6d8f1db
Torben <59419684+entorb@users.noreply.github.com>
2025-12-04 06:46:58 +0100

    minor: text

--- shared/texts.py
+++ shared/texts.py
@@ -4,3 +4,6 @@ GOOGLE_DISCLAIMER = (
     "***Achtung***: *Die Google KI wird deine Eingaben zum Trainieren verwenden."
     " Nur für Dinge verwenden, die nicht streng geheim sind.*"
 )
+
+LABEL_MY_TEXT = "Mein Text"
+LABEL_KI_TEXT = "KI Text"
