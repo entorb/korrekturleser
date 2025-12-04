19c3c47b6cf88622526221c60923dfb2019b64d7
Torben <59419684+entorb@users.noreply.github.com>
2025-12-04 06:48:02 +0100

    deploy.sh: only fastapi+vue

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,10 +7,8 @@ rm -f */.DS_Store
 
 
 # 1. Frontend
-# ensure that it is running
-# else: ./scripts/fastapi.sh
-sh ./scripts/fastapi.sh
-pnpm run generate-api || exit 1
+# ensure that fastapi is running, else start it
+pnpm run generate-api || ./scripts/fastapi.sh & pnpm run generate-api
 pnpm run check || exit 1
 pnpm run build || exit 1
 rsync -ruzv --no-links --delete --delete-excluded dist/* entorb@entorb.net:html/korrekturleser-vue/
@@ -22,18 +20,18 @@ python3 scripts/config_convert.py
 ./scripts/ruff.sh || exit 1
 ./scripts/pytest.sh || exit 1
 
-rsync -uz .streamlit/config-prod.toml entorb@entorb.net:korrekturleser/.streamlit/config.toml
+# rsync -uz .streamlit/config-prod.toml entorb@entorb.net:korrekturleser/.streamlit/config.toml
 rsync -uz requirements.txt entorb@entorb.net:korrekturleser/
 rsync -uz .env entorb@entorb.net:korrekturleser/
-rsync -uz pyproject.toml entorb@entorb.net:korrekturleser/
+# rsync -uz pyproject.toml entorb@entorb.net:korrekturleser/
 rsync -ruzv --no-links --delete --delete-excluded --exclude __pycache__ shared/ entorb@entorb.net:korrekturleser/shared/
-rsync -ruzv --no-links --delete --delete-excluded --exclude __pycache__ streamlit_app/ entorb@entorb.net:korrekturleser/streamlit_app/
+# rsync -ruzv --no-links --delete --delete-excluded --exclude __pycache__ streamlit_app/ entorb@entorb.net:korrekturleser/streamlit_app/
 rsync -ruzv --no-links --delete --delete-excluded --exclude __pycache__ fastapi_app/ entorb@entorb.net:korrekturleser/fastapi_app/
 
 echo installing packages
 ssh entorb@entorb.net "pip3.11 install --user -r korrekturleser/requirements.txt > /dev/null"
 
-echo restarting korrekturleser-streamlit
-ssh entorb@entorb.net "supervisorctl restart korrekturleser-streamlit"
+# echo restarting korrekturleser-streamlit
+# ssh entorb@entorb.net "supervisorctl restart korrekturleser-streamlit"
 echo restarting korrekturleser-fastapi
 ssh entorb@entorb.net "supervisorctl restart korrekturleser-fastapi"
