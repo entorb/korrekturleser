77da6963092dea444a19cb566f8cb0299494d865
Torben <59419684+entorb@users.noreply.github.com>
2026-02-16 19:21:50 +0100

    Update tool config

---
+++ scripts/deploy.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+# ensure we are in the root dir
+cd $(dirname $0)/..
+
+# exit upon error
+set -e
+
+# cleanup
+rm -f .DS_Store
+rm -f */.DS_Store
+
+# ruff
+uv run ruff format
+uv run ruff check
+
+# 1. Frontend
+pnpm run generate-api
+pnpm run check
+pnpm run build
+rsync -ruzv --no-links --delete --delete-excluded dist/* entorb@entorb.net:html/korrekturleser-vue/
+
+
+# 2. Backends
+# config.toml -> config-prod.toml
+python3 scripts/config_convert.py
+./scripts/run_ruff.sh
+./scripts/run_pytest.sh
+
+# rsync -uz .streamlit/config-prod.toml entorb@entorb.net:korrekturleser/.streamlit/config.toml
+rsync -uz requirements.txt entorb@entorb.net:korrekturleser/
+
+# DO NOT sync the .env secret file any more
+# rsync -uz .env entorb@entorb.net:korrekturleser/
+
+# rsync -uz pyproject.toml entorb@entorb.net:korrekturleser/
+rsync -ruzv --no-links --delete --delete-excluded --exclude __pycache__ shared/ entorb@entorb.net:korrekturleser/shared/
+# rsync -ruzv --no-links --delete --delete-excluded --exclude __pycache__ streamlit_app/ entorb@entorb.net:korrekturleser/streamlit_app/
+rsync -ruzv --no-links --delete --delete-excluded --exclude __pycache__ fastapi_app/ entorb@entorb.net:korrekturleser/fastapi_app/
+
+echo installing packages
+ssh entorb@entorb.net "pip3.11 install --user -r korrekturleser/requirements.txt > /dev/null"
+
+# echo restarting korrekturleser-streamlit
+# ssh entorb@entorb.net "supervisorctl restart korrekturleser-streamlit"
+echo restarting korrekturleser-fastapi
+ssh entorb@entorb.net "supervisorctl restart korrekturleser-fastapi"
+
+echo DONE
---
+++ scripts/update.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# ensure we are in the root dir
+cd $(dirname $0)/..
+
+# exit upon error
+set -e
+
+# 1. Python
+
+uv remove pandas pyarrow google-genai mysql-connector-python st_copy streamlit azure-identity bcrypt dotenv fastapi nicegui openai pydantic pyjwt slowapi
+uv remove --dev ruff ollama pre-commit pytest pytest-cov tomli-w watchdog uvicorn
+
+uv lock --upgrade
+uv sync --upgrade
+
+# pin to old versions due to Uberspace restrictions
+uv add pandas==2.2.3 pyarrow==20.0.0 mysql-connector-python==9.4.0 google-genai st_copy streamlit azure-identity bcrypt dotenv fastapi nicegui openai pydantic pyjwt slowapi
+uv add --dev ruff ollama pre-commit pytest pytest-cov tomli-w watchdog uvicorn
+
+uv lock --upgrade
+uv sync --upgrade
+
+python scripts/gen_requirements.py
+
+# ruff
+uv run ruff check --fix
+uv run ruff format
+
+# pre-commit
+uv run pre-commit autoupdate
+uv run pre-commit run --all-files
+
+# 2. Vue
+
+# remove old node_modules
+rm -rf node_modules
+
+pnpm up
+pnpm run check
+pnpm run generate-api
+
+echo DONE
