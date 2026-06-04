8b0323be3a1d2b6a581c124383ca8802fa33e0e6
Torben <59419684+entorb@users.noreply.github.com>
2026-06-04 07:00:28 +0200

    generated files

--- vue_app/src/api/client/utils.gen.ts
+++ vue_app/src/api/client/utils.gen.ts
@@ -83,14 +83,12 @@ const checkForExistence = (
   return false
 }
 
-export const setAuthParams = async ({
-  security,
-  ...options
-}: Pick<Required<RequestOptions>, 'security'> &
-  Pick<RequestOptions, 'auth' | 'query'> & {
+export async function setAuthParams(
+  options: Pick<RequestOptions, 'auth' | 'query' | 'security'> & {
     headers: Record<any, unknown>
-  }) => {
-  for (const auth of security) {
+  }
+): Promise<void> {
+  for (const auth of options.security ?? []) {
     if (checkForExistence(options, auth.name)) {
       continue
     }
