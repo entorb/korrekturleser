0a07bc4c3d5f027e981cdc4344bddf427f68065b
Torben <59419684+entorb@users.noreply.github.com>
2026-08-13 21:40:48 +0200

    md lint and cspell via npm dlx

--- scripts/chk_js_format.sh
+++ scripts/chk_js_format.sh
@@ -4,6 +4,7 @@
 cd "$(dirname "$0")/.."
 
 # pnpm run format
-pnpm exec biome check --write .
-
-if [ $? -ne 0 ]; then exit 1; fi
+fail=0
+pnpm exec biome format --write . || fail=1
+pnpm exec biome check --write . || fail=1
+exit $fail
--- scripts/chk_spelling.sh
+++ scripts/chk_spelling.sh
@@ -4,9 +4,9 @@
 cd "$(dirname "$0")/.."
 
 rm -f cspell-words-missing.txt
-pnpm exec cspell-cli --cache --gitignore --quiet --unique .
+pnpm dlx cspell-cli@10.0.1 --cache --gitignore --quiet --unique .
 if [ $? -ne 0 ]; then
-    pnpm exec cspell-cli --cache --gitignore --unique --words-only . > cspell-words-missing.txt
+    pnpm dlx cspell-cli@10.0.1 --cache --gitignore --unique --words-only . > cspell-words-missing.txt
     echo "See cspell-words-missing.txt for unknown words. Fix or transfer to cspell-words.txt"
     exit 1
 fi
