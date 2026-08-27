548e727456b15ef3cbbbfe759fb76692b74e515a
Torben <59419684+entorb@users.noreply.github.com>
2026-08-27 10:06:08 +0200

    Update chk_pre-commit.sh

--- scripts/chk_pre-commit.sh
+++ scripts/chk_pre-commit.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")/.."
 out=$(mktemp)
 trap 'rm -f "$out"' EXIT INT TERM
 
-prek run --all-files >"$out" 2>&1
+prek run --all-files --quiet >"$out" 2>&1
 status=$?
 
 if [ $status -ne 0 ]; then
